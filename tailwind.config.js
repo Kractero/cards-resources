d75d531104b90ca7d4955735e8ec86585202310d
Kractero <115837402+Kractero@users.noreply.github.com>
2023-09-22 00:45:11 -0700

    css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {
+      colors: {
+        background: 'rgb(var(--color-primary) / <alpha-value>)',
+        content: 'rgb(var(--color-secondary) / <alpha-value>)',
+        tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
+        textprimary: 'rgb(var(--text-primary) / <alpha-value>)',
+        textcontent: 'rgb(var(--text-content) / <alpha-value>)',
+        cardprimarytext: 'rgb(var(--card-text-primary) / <alpha-value>)',
+        cardsecondarytext: 'rgb(var(--card-text-secondary) / <alpha-value>)',
+        label: 'rgb(var(--labels) / <alpha-value>)',
+        herobuttons: 'rgb(var(--herobuttons) / <alpha-value>)',
+        herobuttonshover: 'rgb(var(--herobuttons-hover) / <alpha-value>)'
+      },
       fontFamily: {
         notosans: ['Noto Sans', 'sans-serif']
       }
